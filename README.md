bab7d5ec091d700a4c2a13d43a3952906d1626f1
stannard95 <14066588@brookes.ac.uk>
2017-10-26 14:16:18 +0100

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# mongodb-server
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: mongodb-server
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
